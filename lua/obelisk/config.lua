e4ac7f6114003fc7f9a63d659a9f83b898ed5581
Aditya Azad <adityaazad121@gmail.com>
2026-07-17 17:05:03 -0400

    batman

---
+++ lua/obelisk/config.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+---@class obelisk.Config
+---@field greeting string Message shown by the :Obelisk command
+
+M.defaults = {
+    greeting = "hello",
+}
+
+return M
---
+++ lua/obelisk/init.lua
@@ -0,0 +1,17 @@
+local config = require("obelisk.config")
+
+local M = {}
+
+---@type obelisk.Config
+M.config = {}
+
+---@param opts? obelisk.Config
+function M.setup(opts)
+    M.config = vim.tbl_deep_extend("force", config.defaults, opts or {})
+
+    vim.api.nvim_create_user_command("Obelisk", function()
+        vim.notify("obelisk: " .. M.config.greeting, vim.log.levels.INFO)
+    end, { desc = "Obelisk demo command" })
+end
+
+return M
